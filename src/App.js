1ef583f0dacc88451016a2a05ef220479d3c473b
Tarek <tarekhassan3874@gmail.com>
2021-10-22 19:53:39 +0200

    feat: created sidebar
    saidebar is functional and routing is working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
-      <div className='layout-container'>
-        <Sidebar />
-        <Home />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <div className="layout-container">
+              <Sidebar current='home'/>
+              <Home />
+            </div>
+          </Route>
+          
+          <Route path='/water-contracts'>
+            <div className="layout-container">
+              <Sidebar current='water-contracts'/>
+              <div></div>
+            </div>
+          </Route>
+          
+          <Route path='/malfunctions'>
+            <div className="layout-container">
+              <Sidebar current='malfunctions'/>
+              <div></div>
+            </div>
+          </Route>
+          
+          <Route path='/responses'>
+            <div className="layout-container">
+              <Sidebar current='responses'/>
+              <div></div>
+            </div>
+          </Route>
+          
+          <Route path='/monthly-readings'>
+            <div className="layout-container">
+              <Sidebar current='monthly-readings'/>
+              <div></div>
+            </div>
+          </Route>
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,68 @@
-import React from 'react';
-function Sidebar() {
-    return (
-        <div className='sidebar-container'>
-            <div className='sidebar'>
-                <h1>Sidebar</h1>
+import React from "react";
+import {
+  FaHome,
+  FaWater,
+  FaWrench,
+  FaReplyAll,
+  FaFileAlt,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+function Sidebar(props) {
+  const { current } = props;
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <h2>Dshboard</h2>
+
+        <div className="pages">
+          <Link to="/">
+            <div className={current === "home" ? "page active-page" : "page"}>
+              <FaHome />
+              <p className="arabic">الرئيسية</p>
             </div>
+          </Link>
+          <Link to="/water-contracts">
+            <div
+              className={
+                current === "water-contracts" ? "page active-page" : "page"
+              }
+            >
+              <FaWater />
+              <p className="arabic">عقود المياه</p>
+            </div>
+          </Link>
+          <Link to="/malfunctions">
+            <div
+              className={
+                current === "malfunctions" ? "page active-page" : "page"
+              }
+            >
+              <FaWrench />
+              <p className="arabic">الأعطال</p>
+            </div>
+          </Link>
+          <Link to="/responses">
+            <div
+              className={current === "responses" ? "page active-page" : "page"}
+            >
+              <FaReplyAll />
+              <p className="arabic">الردود</p>
+            </div>
+          </Link>
+          <Link to="/monthly-readings">
+            <div
+              className={
+                current === "monthly-readings" ? "page active-page" : "page"
+              }
+            >
+              <FaFileAlt />
+              <p className="arabic">القراءات الشهرية</p>
+            </div>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
